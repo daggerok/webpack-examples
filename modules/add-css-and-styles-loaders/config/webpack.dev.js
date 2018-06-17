2175ae7e19821a88d20458ab0feeb5429312b3dd
Maksim Kostromin <daggerok@gmail.com>
2018-06-17 16:54:19 +0300

    The same thing we do every night, Pinky - try to take over the world! (C) whatthecommit.com

---
+++ modules/add-css-and-styles-loaders/config/webpack.dev.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve, join } = require('path');
+
+// tag::content[]
+module.exports = {
+  // ...
+  // end::content[]
+  entry: {
+    main: './src/index.js'
+  },
+  mode: 'development',
+  output: {
+    filename: '[name]-bundle.js',
+    path: resolve(__dirname, '../dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/favicon.ico',
+    }),
+  ],
+  devServer: {
+    contentBase: join(__dirname, '../dist'),
+  },
+  // tag::content[]
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
+      },
+    ],
+  },
+};
+// end::content[]
---
+++ modules/add-css-and-styles-loaders/src/index.js
@@ -0,0 +1,5 @@
+// tag::content[]
+require('./styles.css');
+// end::content[]
+
+console.log('hey!');

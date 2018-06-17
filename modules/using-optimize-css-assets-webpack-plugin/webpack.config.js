7e63d02cd671f538559a0ca68230018f08d49127
Maksim Kostromin <daggerok@gmail.com>
2018-06-18 02:26:59 +0300

    add optimize-css-assets-webpack-plugin usage example module | arrrggghhhhh fixed! (C) whatthecommit.com

---
+++ modules/using-optimize-css-assets-webpack-plugin/webpack.config.js
@@ -0,0 +1,127 @@
+const { resolve } = require('path');
+const dist = resolve(__dirname, 'dist');
+const mode = process.env.NODE_ENV || 'production';
+const isProduction = mode === 'production';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// tag::content[]
+const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  // ...
+  // end::content[]
+  mode,
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: '[name]-[contenthash].js',
+    path: dist,
+  },
+  devServer: {
+    contentBase: dist,
+    overlay: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 },
+          },
+          'postcss-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 2 },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+            },
+          },
+          'less-loader',
+        ],
+      },
+      {
+        test: /\.s(a|c)ss$/i,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 2 },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.styl$/i,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 2 },
+          },
+          'postcss-loader',
+          'stylus-loader',
+        ],
+      },
+      {
+        test: /\.(ico|jpg|png|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]-[contenthash].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  // tag::content[]
+  plugins: [
+    isProduction ? new OptimizeCssPlugin() : undefined,
+    // end::content[]
+    new MiniCssExtractPlugin({
+      filename: '[name]-[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/favicon.ico',
+      minify: isProduction ? {
+        minifyJS: true,
+        minifyCSS: true,
+        quoteCharacter: '"',
+        decodeEntities: true,
+        removeComments: true,
+        useShortDoctype: true,
+        collapseWhitespace: true
+      } : {},
+    }),
+    // tag::content[]
+  ].filter(p => !!p),
+};
+// end::content[]
